5a199dc5a99bc3fcd54098b54f27e38ab554579b
ZainAli - Sadiq <125101944+ZainAli82@users.noreply.github.com>
2023-06-06 10:30:46 -0500

    Update testGoogle.py

--- testGoogle.py
+++ testGoogle.py
@@ -4,7 +4,7 @@ import boto3
 import six
 from google.cloud import translate_v2 as translate
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/zaine/SadiqProjects/ml-kit-algo/translationtest-380502-cf110ae29be7.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
 def translate_text(target, text):
     translate_client = translate.Client()
